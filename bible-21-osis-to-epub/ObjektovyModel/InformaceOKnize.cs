e0934631d4fe2f42a079f3c523807e7944887441
Martin Rotter <rotter@praktik.cz>
2018-03-22 12:24:11 +0100

    Chybějící soubor.

---
+++ bible-21-osis-to-epub/ObjektovyModel/InformaceOKnize.cs
@@ -0,0 +1,40 @@
+﻿namespace BibleDoEpubu.ObjektovyModel
+{
+  internal class InformaceOKnize
+  {
+    #region Vlastnosti
+
+    public string CeskaZkratka
+    {
+      get;
+    }
+
+    public string Nadpis
+    {
+      get;
+    }
+
+    #endregion
+
+    #region Konstruktory
+
+    public InformaceOKnize(string zkratka, string nadpis)
+    {
+      CeskaZkratka = zkratka;
+      Nadpis = nadpis;
+    }
+
+    #endregion
+
+    #region Metody
+
+    public static implicit operator InformaceOKnize(string zkratkaNadpis)
+    {
+      int indexStrednik = zkratkaNadpis.IndexOf(';');
+
+      return new InformaceOKnize(zkratkaNadpis.Substring(0, indexStrednik), zkratkaNadpis.Substring(indexStrednik + 1));
+    }
+
+    #endregion
+  }
+}
